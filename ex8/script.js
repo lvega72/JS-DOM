b09a8fefbc677d793f9ed51340a8f9b0c82848a2
lvega <lvega@lvega.com>
2019-10-15 17:52:58 -0500

    additions to ex8

--- ex8/script.js
+++ ex8/script.js
@@ -8,13 +8,38 @@
 
 document.getElementById("add-button").addEventListener('click', function() {
     
-    // get the text entered by the user
-    let todoText = document.getElementById("description").value;
-
-    // create a <li> element with the text
+    // create a <li> element 
     let toDoLineItemElement = document.createElement("li");
-    toDoLineItemElement.innerHTML = todoText;
     
+    // create "delete" <button> element and add it to <li> element
+    let deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.addEventListener('click', function() {
+        document.getElementById("todo-list").removeChild(
+            this.parentElement
+        );
+    })
+    toDoLineItemElement.appendChild(deleteButton);
+
+     // create "done" <button> element and add it to <li> element
+     let doneButton = document.createElement("button");
+     doneButton.innerText = "Done";
+     doneButton.addEventListener('click', function() {
+         let todoText = this.nextElementSibling; 
+         todoText.style.textDecoration = "line-through";
+         todoText.style.color = "blue";
+     })
+     toDoLineItemElement.appendChild(doneButton  );
+    
+
+    // create a <p> element with the text entered by the user and
+    // add it to the <li> element
+    let todoText = document.getElementById("description").value;
+    let todoParagraph = document.createElement("p");
+    todoParagraph.innerHTML = todoText; 
+    todoParagraph.contentEditable = true;
+    
+    toDoLineItemElement.appendChild(todoParagraph);
 
     // add the <li> element to the todo-list element
     document.getElementById("todo-list").appendChild(toDoLineItemElement);
